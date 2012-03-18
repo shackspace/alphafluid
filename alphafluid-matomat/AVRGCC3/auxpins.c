4f93a0965a3c243c9b0ef8ab1bac4c0f927d7946
armageddon421 <armageddon421@googlemail.com>
2012-03-18 01:18:53 +0100

    added files for the auxpins

---
+++ alphafluid-matomat/AVRGCC3/auxpins.c
@@ -0,0 +1,6 @@
+/*
+ * auxpins.c
+ *
+ * Created: 17.03.2012 21:50:56
+ *  Author: armageddon
+ */ 
---
+++ alphafluid-matomat/AVRGCC3/auxpins.h
@@ -0,0 +1,19 @@
+/*
+ * auxpins.h
+ *
+ * Created: 17.03.2012 21:51:11
+ *  Author: armageddon
+ */ 
+
+
+#ifndef AUXPINS_H_
+#define AUXPINS_H_
+
+#define AUX_PORT	PORTC
+#define AUX_PIN		PINC
+
+#define AUX_DOOR	0
+
+
+
+#endif /* AUXPINS_H_ */
