fb23bdd595159474b73630ccfbed5917e47e433a
tensau <github@tutschonwieder.net>
2013-08-31 21:02:53 +0200

    LICK API-Calls auf API v1.0 angepasst

--- alphafluid-server/server.py
+++ alphafluid-server/server.py
@@ -81,21 +81,21 @@ def mat_checkambient():
 def send_bought(st):
 	return 42	#temporarily disable lick interface
 	try:
-		urllib2.urlopen("https://appserv.tutschonwieder.net:8443/apex/prod/sellProduct?apikey="+apikey+"&automat_id=1&schacht_id=" + str(mapping[int(st)]) + "&anzahl=1")
+		urllib2.urlopen("http://ora2.tutschonwieder.net:8081/apex/tensai-prod/put/sellproduct/"+apikey+"/1/" + str(mapping[int(st)]) + "/1")
 	except:
 		log("!!!!!!!!!!!!!!!!!!!!!! LICK DOWN !!!!!!!!!!!!!!!!!!!!!")
 
 def send_empty(st):
 	return 42	#temporarily disable lick interface
 	try:
-		urllib2.urlopen("https://appserv.tutschonwieder.net:8443/apex/prod/schachtLeer?apikey="+apikey+"&automat_id=1&schacht_id=" + str(mapping[int(st)]))
+		urllib2.urlopen("http://ora2.tutschonwieder.net:8081/apex/tensai-prod/put/schachtleer/"+apikey+"/1/" + str(mapping[int(st)]))
 	except:
 		log("!!!!!!!!!!!!!!!!!!!!!! LICK DOWN !!!!!!!!!!!!!!!!!!!!!")
 
 def lick_get_level(shaft):
 	return 42	#temporarily disable lick interface
 	try:
-		lines = urllib2.urlopen("https://appserv.tutschonwieder.net:8443/apex/prod/getFuellstand?automat_id=1&schacht_id="+str(shaft)).readlines()
+		lines = urllib2.urlopen("http://ora2.tutschonwieder.net:8081/apex/tensai-prod/get/fuellstand/1/"+str(shaft)).readlines()
 		for line in lines:
 			print line
 			#if line.startswith('{"\"tensai-prod\".lick_api.getfuellstand(/*in:automat_id*/:1,/*in:schacht_id*/:2)":'):
